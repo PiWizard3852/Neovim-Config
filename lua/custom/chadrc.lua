af011edcce18121ceefacee6b5066f0dd1cabe68
Owen Wood <owenwood0101@icloud.com>
2023-08-30 09:01:18 -0400

    feat: change theme toggle

--- lua/custom/chadrc.lua
+++ lua/custom/chadrc.lua
@@ -3,8 +3,8 @@ local M = {}
 local highlights = require 'custom.highlights'
 
 M.ui = {
-  theme = 'penumbra_light',
-  theme_toggle = { 'penumbra_light', 'nightfox' },
+  theme = 'nightfox',
+  theme_toggle = { 'nightfox', 'palenight' },
 
   hl_override = highlights.override,
   hl_add = highlights.add,
